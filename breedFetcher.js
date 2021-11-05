a63d1f04ad8fcce7d3563a50b886f62fb59d4b5f
Taha Helwani <telhe008@uottawa.ca>
2021-11-05 02:51:29 +0000

    Added Mocha/Chai tests in a separate test file and refactored the code in breedFetcher.js by moving the main request logic into a function named fetchBreedDescription and moved all the user-facing logic to index.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,21 @@
 const request = require('request');
-const args = process.argv[2];
-// console.log(args);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log("typeof body", typeof body)
-  // console.log(data);
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('The requested breed is not found');
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      const description = data[0].description;
-      console.log(description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback('The requested breed is not found', null);
+      } else {
+        const description = data[0].description;
+        callback(null, description);
 
+      }
     }
-  }
-  // console.log("typeof data", typeof data);
-});
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
